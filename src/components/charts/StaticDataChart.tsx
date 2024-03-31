1f7c795d26259d29eb66e69a0943f3dcc15c4ea0
Filip Burski <92650529+FlickJS@users.noreply.github.com>
2024-03-31 15:47:38 +0200

    Update StaticDataChart.tsx

--- src/components/charts/StaticDataChart.tsx
+++ src/components/charts/StaticDataChart.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import React, { useRef, useEffect } from "react";
+import Chart from "chart.js/auto";
 
 interface StaticData {
   [year: string]: {
@@ -9,9 +10,55 @@ interface StaticData {
 }
 
 const StaticDataChart: React.FC<{ data: StaticData[] }> = ({ data }) => {
+  const chartRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    const processedData = data.flatMap((object) =>
+      Object.entries(object).flatMap(([year, months]) =>
+        Object.entries(months).flatMap(([month, days]) =>
+          Object.entries(days).map(([day, value]) => ({
+            date: `${day}.${month}.${year}`,
+            value,
+          }))
+        )
+      )
+    );
+
+    const chartData = {
+      labels: processedData.map((d) => d.date),
+      datasets: [
+        {
+          label: "Amount of errors",
+          data: processedData.map((d) => d.value),
+          backgroundColor: "#646cff",
+          borderColor: "#646cff",
+          borderWidth: 1,
+        },
+      ],
+    };
+
+    const config = {
+      type: "bar",
+      data: chartData,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    };
+
+    const myChart = new Chart(chartRef.current.getContext("2d"), config);
+
+    return () => {
+      myChart.destroy();
+    };
+  }, [data]);
+
   return (
-    <div>
-      <h2>Statistics</h2>
+    <div style={{ width: "800px" }}>
+      <canvas ref={chartRef}></canvas>
     </div>
   );
 };
